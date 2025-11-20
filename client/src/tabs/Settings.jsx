bd054dcf22dd7e1cc43ac40123bf075396406160
John Christian Vicente <johnc.vicente1@gmail.com>
2025-11-20 10:44:55 +0800

    changed status to system

---
+++ client/src/tabs/Settings.jsx
@@ -0,0 +1,10 @@
+const Settings = () => {
+    return (
+        <div>
+            <h1>Settings</h1>
+            <p>System settings will be displayed here.</p>
+        </div>
+    );
+};
+
+export default Settings;
