aea06e2613e5f8b2e9354a89988a4e7fb45b90e8
John Christian Vicente <johnc.vicente1@gmail.com>
2025-11-27 22:19:48 +0800

    Added routing for analytics

--- server/server.js
+++ server/server.js
@@ -3,9 +3,13 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
+import analyticsRouter from "./routes/AnalyticsRoute.js";
+
 const app = express();
 await connectDB();
 app.use(cors());
+
+app.use("/api/analytics", express.json(), analyticsRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
